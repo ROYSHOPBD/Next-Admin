e60a479b7b152daafd11c48c2256655a7051ec63
ROYSHOPBD <royshopbd00@gmail.com>
2024-02-22 01:50:25 +0600

    add many css

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,7 +1,32 @@
+"use client";
+import { usePathname } from "next/navigation";
 import React from "react";
+import style from "./navber.module.css";
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const pathname = usePathname();
+  return (
+    <div className={style.container}>
+      <div className={style.title}>{pathname.split("/").pop()}</div>
+      <div className={style.menu}>
+        <div className={style.search}>
+          <MdSearch />
+          <input type="text" placeholder="Search...." className={style.input} />
+        </div>
+        <div className={style.icons}>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
